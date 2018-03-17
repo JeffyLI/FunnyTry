5c8533aefac7c9763d4a4bd786ef37b4a41e5419
JeffyLI <780446259@qq.com>
2018-03-17 09:40:34 +0800

    Update dictionaryOnline.py

--- Dictionary/dictionaryOnline.py
+++ Dictionary/dictionaryOnline.py
@@ -45,7 +45,45 @@ def print_result(html):
         else:
             out+=x+" "
     print(out)
-    print(" ")    
+    print(" ")
+
+#保存查询的结果到文件
+def saveHtmlToFile(filename,content,html):
+    savefile=open(filename,'a')
+    relink=re.compile("(.+?)&nbsp;&nbsp;")
+    out=""
+    savefile.write(content)
+    for x in relink.findall(html):
+        if("br" in x):
+            savefile.write(";"+out)
+            out=x.split(">")[1]
+        else:
+            out+=x+" "
+    if out=="":
+        savefile.close()
+        return
+    savefile.write(";"+out+"\n")
+    savefile.close()
+
+#批量查询单词和保存结果
+def save_searchs(searchfilename,savefilename):
+    url="http://dict-co.iciba.com/search.php"
+    file=open(searchfilename,'r')
+    for x in file.readlines():
+        try:
+            data={}
+            content=x.strip()
+            data['word']=content
+            data['submit']='提交'
+            data = urllib.parse.urlencode(data).encode('utf-8')
+            response = urllib.request.urlopen(url, data)
+            html = response.read().decode('utf-8')
+            saveHtmlToFile(savefilename,content,html)
+            print(content)
+        except:
+            print("Error in :"+x)
+    file.close()
+    print("finish")
 
 
 if __name__ =="__main__":
